198eab0c215a0b0682a9264d537cc9a6c3ee4b3d
pranav s <dev.pranabsk@gmail.com>
2022-08-03 17:16:52 +0530

    Button & Date-Time Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import NavigationIcon from '@material-ui/icons/Navigation';
+import { CheckBox } from '@material-ui/icons';
+import { FormControlLabel } from '@material-ui/core';
+// fot data & time
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Button</h2>
+     <h3>Text Button</h3>
+     <Button>Click</Button>
+     <Button color="secondary">Click</Button>
+     <Button href="#" color="inherit">Link</Button>
+     <Button disabled>Click</Button>
+     <h3>Contained Button</h3>
+     <Button variant="contained">Click</Button>
+     <Button variant="contained" color="secondary">Click</Button>
+     <Button variant="contained" href="#" color="inherit">Link</Button>
+     <Button variant="contained" disabled>Click</Button>
+     <h3>Outlined Button</h3>
+     <Button variant="outlined">Click</Button>
+     <Button variant="outlined" color="secondary">Click</Button>
+     <Button variant="outlined" href="#" color="inherit">Link</Button>
+     <Button variant="outlined" disabled>Click</Button>
+     <h3>Button Size</h3>
+     <Button variant="contained" size="small">Click</Button>
+     <Button variant="contained" color="secondary" size="medium">Click</Button>
+     <Button variant="contained" href="#" color="inherit" size="large">Link</Button>
+     <h3>Button with Icon</h3>
+     <Button variant="contained" color="secondary" startIcon={<DeleteIcon/>}>Delete</Button>
+     <Button variant="contained" color="secondary" endIcon={<DeleteIcon/>}>Delete</Button>
+     <h3>Icon Button</h3>
+     <IconButton variant="contained" color="primary"><DeleteIcon/></IconButton>
+     <h3>Custom Button</h3>
+     <Button variant="contained" style={{
+      backgroundColor: "green",
+      color: "white"
+     }}>Custom</Button>
+     <h3>Button Group</h3>
+     <ButtonGroup variant="contained" color="primary">
+      <Button>One</Button>
+      <Button>Two</Button>
+      <Button>Three</Button>
+     </ButtonGroup>
+     <h3>Vertical Button Group</h3>
+     <ButtonGroup orientation="vertical" color="primary">
+      <Button>One</Button>
+      <Button>Two</Button>
+      <Button>Three</Button>
+     </ButtonGroup>
+     <h3>Floating Action Button</h3>
+     <Fab color="primary"><AddIcon/></Fab>
+     <Fab color="secondary"><EditIcon/></Fab>
+     <Fab color="extended"><NavigationIcon/>Navigate</Fab>
+     <Fab disabled><FavoriteIcon/></Fab>
+     <h3>Checkbox</h3>
+     <CheckBox/>
+     <CheckBox checked="true" color="secondary"/>
+     <CheckBox checked="true" color="primary"/>
+     <CheckBox defaultChecked indeterminate/>
+     <FormControlLabel control={
+      <CheckBox checked="true" name="checkbox"/>
+     }label="Secondary"/>
+     <hr/>
+    <h2>Date & Time</h2>
+    <form className={classes.container} noValidate>
+   <TextField
+    id="date"
+    label="Birthday"
+    type="date"
+    defaultValue="2017-05-24"
+    className={classes.textField}
+    InputLabelProps={{
+      shrink: true,
+    }}
+  />
+</form>
+
     </div>
   );
 }
