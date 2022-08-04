1a6f5c8692d3bd3d563b136e32c63ed5a46c3ee8
pranav s <dev.pranabsk@gmail.com>
2022-08-04 10:41:45 +0530

    Dialog

--- src/App.js
+++ src/App.js
@@ -60,7 +60,11 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Rating from '@material-ui/lab/Rating';
 import Link from '@material-ui/core/Link';
 import Alert from '@material-ui/lab/Alert';
-
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 // for data & time
 const useStyles = makeStyles((theme) => ({
@@ -99,6 +103,18 @@ function App() {
   const handleChange = (event) => {
     setValue(event.target.value);
   };
+
+ // for dialog
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
   return (
     <div className="App">
      <h2>Button</h2>
@@ -581,6 +597,36 @@ function App() {
   <Alert onClose={() => setShow(false)} severity="info">This is alert— check it out!</Alert>
    )}
    </Box>
+   <h2>Dialog</h2>
+   <span>use alert dialog</span>
+   <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous location data to
+            Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+   <hr/>
     </div>
 
   );
